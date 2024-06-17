415b838b94c5d917851162c35f9e19928f473f55
sequoiaforty <152109401+sequoiaforty@users.noreply.github.com>
2024-06-17 00:19:23 -0400

    US08 Formatting
    Formatted US08 to return a list of string errors

--- Group1.py
+++ Group1.py
@@ -272,6 +272,8 @@ def US07Validation():
                 
 # US08 Birth before Marriage of parents
 def US08Validation():
+    errors = []
+
     for fam_id in g_FamDict.keys():
         if "MARR" in g_FamDict[fam_id]:
             marriageDT = datetime.strptime(g_FamDict[fam_id]["MARR"], "%d %b %Y")
@@ -281,11 +283,15 @@ def US08Validation():
                     birth_date = datetime.strptime(g_IndiDict[child_id]["BIRT"], "%d %b %Y")
                     # Emphasize birth before marriage
                     if birth_date < marriageDT:
-                        AppendDictStr("ERROR", g_FamDict[fam_id], f"ERROR: US08: {g_IndiDict[child_id]['NAME']} ({child_id}) born before parents' marriage in family {fam_id}", "\n")
+                        #AppendDictStr("ERROR", g_FamDict[fam_id], f"ERROR: US08: {g_IndiDict[child_id]['NAME']} ({child_id}) born before parents' marriage in family {fam_id}", "\n")
+                        errors.append(f"ERROR: US08: {g_IndiDict[child_id]['NAME']} ({child_id}) born before parents' marriage in family {fam_id}.")
         else:
             for child_id in g_FamDict[fam_id].get("CHIL", []):
                 if child_id in g_IndiDict and "BIRT" in g_IndiDict[child_id]:
-                    AppendDictStr("ERROR", g_FamDict[fam_id], f"ERROR: US08: {g_IndiDict[child_id]['NAME']} ({child_id}) has no recorded marriage date for parents in family {fam_id}", "\n")
+                    #AppendDictStr("ERROR", g_FamDict[fam_id], f"ERROR: US08: {g_IndiDict[child_id]['NAME']} ({child_id}) has no recorded marriage date for parents in family {fam_id}", "\n")
+                    errors.append(f"ERROR: US08: {g_IndiDict[child_id]['NAME']} ({child_id}) has no recorded marriage date for parents in family {fam_id}")
+
+    return errors
 
 def DataValidation():
     US01Validation()
